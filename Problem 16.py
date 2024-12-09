dbb8091410661b41e94de301c764cf6bca857230
Teddy <teddymcdermott5344@gmail.com>
2024-12-09 21:57:25 +0000

    Started Problem 16

---
+++ Problem 16.py
@@ -0,0 +1,11 @@
+# Power Digit Sum
+# 2^15 = 32768
+#The sum of its digits is 3 + 2 + 7 + 6 + 8 = 26
+#What is the sum of the digits of 2^1000
+
+import numpy as np
+
+val = np.power(2,8)
+print(val)
+
+print("Hello")
